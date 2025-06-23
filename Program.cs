3599ae6da202ce1e0d1ff9909373b86c950d5bae
BloodyRoom <sabadashkamaksym@gmail.com>
2025-06-24 00:14:03 +0300

    hm

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Homework_1;
+namespace Homework_1;
+
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        string image = "https://images.panda.org/assets/images/pages/welcome/orangutan_1600x1000_279157.jpg";
+        // Console.WriteLine(await image.UriToBase64());
+        // File.WriteAllText(@"base64.txt", await image.UriToBase64());
+
+        string phone = "0509985114";
+        Console.WriteLine(phone.ToPhoneFormat());
+    }
+}
